2b0d36e2d67fa9b7614e9cc2f60706e6a44a18ec
PSGtatitos <tatitoschris@gmail.com>
2025-04-27 22:04:08 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -2,26 +2,26 @@ calc = input("Choose " \
 "1. Add " \
 "2. Subtract " \
 "3. Multiply " \
-"4. Devide ")
-if calc == "1" or "Add" or "add" or "ADD":
+"4. Divide ")
+if calc == "1" or calc == "Add" or calc == "add" or calc == "ADD":
     number1 = input("First number: ")
     number2 = input("Second number: ")
     number1 = int(number1)
     number2 = int(number2)
     print(number1 + number2)
-if calc == "2" or "Subtract" or "subtract" or "SUBTRACT":
+if calc == "2" or calc == "Subtract" or calc == "subtract" or calc == "SUBTRACT":
     number3 = input("First number: ")
     number4 = input("Second number: ")
     number3 = int(number3)
     number4 = int(number4)
     print(number3 - number4)
-if calc == "3" or "Multiply" or "multiply" or "MULTIPLY":
+if calc == "3" or calc == "Multiply" or calc == "multiply" or calc == "MULTIPLY":
     number5 = input("First number: ")
     number6 = input("Second number: ")
     number5 = int(number5)
     number6 = int(number6)
     print(number5 * number6)
-if calc == "4" or "Devide" or "devide" or "DEVIDE":
+if calc == "4" or calc == "Divide" or calc == "divide" or calc == "DIVIDE":
     number7 = input("First number: ")
     number8 = input("Second number: ")
     number7 = int(number7)
